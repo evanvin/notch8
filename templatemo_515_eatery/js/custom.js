cfefd133b89dd99f5a85ade55121b6834b75e992
Evan Vinciguerra <evanv511@gmail.com>
2022-03-27 14:10:51 -0400

    second commit

--- templatemo_515_eatery/js/custom.js
+++ templatemo_515_eatery/js/custom.js
@@ -21,22 +21,6 @@
           }
     });
 
-    var $grid = $('.beer-grid').isotope({
-      itemSelector: '.beer-item',
-      layoutMode: 'fitRows',
-      getSortData: {
-        name: '.name',
-        symbol: '.symbol',
-        number: '.number parseInt',
-        category: '[data-category]',
-        weight: function( itemElem ) {
-          var weight = $( itemElem ).find('.weight').text();
-          return parseFloat( weight.replace( /[\(\)]/g, '') );
-        }
-      }
-    });
-
-
     // SLIDER
     $('.owl-carousel').owlCarousel({
       animateOut: 'fadeOut',
